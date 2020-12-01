a36e2d6f9754bcf84501dafd19c46f0156662546
Philippe Perret <philippe.perret@yahoo.fr>
2020-12-01 14:33:58 +0100

    Poursuite

--- ajax/ajax/_scripts/crop_score/assemble_systems.rb
+++ ajax/ajax/_scripts/crop_score/assemble_systems.rb
@@ -9,9 +9,13 @@ On ajoute tel écartement en pixels à telle hauteur en pixels
 
 0x{ecartement}+0+{top}
 =end
-SYSTEMES_SEPARATOR = Score.data['system_separator'] || 200 # séparation en pixels entre les système
+SYSTEMS_SEPARATOR = Score.data['system_separator'] || 200 # séparation en pixels entre les système
 
+CODE_APPEND_FIRST_SYSTEM  = '/usr/local/bin/convert %{img1_name} -splice 0x%{gap}+0+0 score_expanded_odd.jpg'
 CODE_APPEND_SYSTEM = '/usr/local/bin/convert %{img1_name} %{img2_name} -append -splice 0x%{gap}+0+%{top} score_expanded_%{odd}.jpg'
+
+# On va profiter pour enregistrer les tops des systèmes courants
+TOPS = []
 
 def assemble_systems
 
@@ -21,30 +25,40 @@ def assemble_systems
   #
 
   code_assemblage = []
-  odd_or_even = 'odd'
-  current_top = 0
+
+  # On doit commencer par ajouter de l'espace au-dessus du premier système
+  code = CODE_APPEND_FIRST_SYSTEM % {img1_name: "score-000.jpg", gap: SYSTEMS_SEPARATOR}
+  code = <<-BASH
+cd "#{FACTORY_FOLDER}"
+#{code}
+
+BASH
+  `#{code}`
+
+  current_top = SYSTEMS_SEPARATOR
+  TOPS << current_top
+
+  odd_or_even = 'even'
+
   current_height = CROP_LINES_DATA[0]['height']
   (0...CROP_LINES_DATA.count).each do |idx|
     data_curline = CROP_LINES_DATA[idx]
     data_nexline = CROP_LINES_DATA[idx + 1]
     current_top += data_curline['height'] # la hauteur de l'image 1
+    TOPS << current_top
     iimg = idx.to_s.rjust(3,'0')
-    if idx == 0
-      curimage_name = "score-#{iimg}.jpg"
-    else
-      curimage_name = "score_expanded_#{odd_or_even == 'odd' ? 'even' : 'odd'}.jpg"
-    end
+    curimage_name = "score_expanded_#{odd_or_even == 'odd' ? 'even' : 'odd'}.jpg"
     neximage_name = "score-#{(idx+1).to_s.rjust(3,'0')}.jpg"
     data = {
       img1_name: curimage_name,
       img2_name: neximage_name,
-      gap: SYSTEMES_SEPARATOR,
+      gap: SYSTEMS_SEPARATOR,
       top: current_top,
       odd: odd_or_even
     }
     code_assemblage << CODE_APPEND_SYSTEM % data
 
-    current_top += SYSTEMES_SEPARATOR
+    current_top += SYSTEMS_SEPARATOR
 
     odd_or_even = odd_or_even == 'odd' ? 'even' : 'odd'
 
@@ -68,7 +82,9 @@ BASH
   score_expanded_path = File.join(FACTORY_FOLDER,score_expanded_name)
   File.delete(autre_expanded_path) if File.exists?(autre_expanded_path)
   # On renomme enfin le fichier final
-  FileUtils.move(score_expanded_path, File.join(FACTORY_FOLDER,'score_expanded.jpg'))
+  FileUtils.move(score_expanded_path, File.join(SCORE_FOLDER,'score_expanded.jpg'))
 
+  # On enregistre les tops dans le fichier de données
+  Score.set(pages: [TOPS])
 
 end #/ assemble_systems
--- ajax/ajax/_scripts/define_score_ini_path.rb
+++ ajax/ajax/_scripts/define_score_ini_path.rb
@@ -4,27 +4,48 @@
   Script qui sauve le code de l'analyse courante
 
 =end
+CODE_CONVERT_APPEND_PAGE = '/usr/local/bin/convert %{images} -append full_score_ini.jpg'
 
-# Pour récupérer la valeur "code" transmise en donnée
-# Par exemple par
-score_ini_path  = Ajax.param(:path)
+def assemble_pages_partition_of_folder(score_ini_path)
+  images = Dir["#{score_ini_path}/*.jpg"].collect{|p|File.basename(p)}.sort
+  code = CODE_CONVERT_APPEND_PAGE % {images: images.join(' ')}
+  code = <<-BASH
+cd "#{score_ini_path}"
+#{code}
+
+  BASH
+  log("code :\n#{code}")
+  res = `#{code}\n`
+  log("Retour de code : #{res.inspect}")
+  return File.join(score_ini_path,'full_score_ini.jpg')
+end #/ assemble_pages_partition_of_folder
 
 begin
+  score_ini_path  = Ajax.param(:path)
 
   if File.exists?(score_ini_path)
 
+    if File.directory?(score_ini_path)
+      # Quand le path est celui d'un dossier, ce dossier doit contenir toutes
+      # les feuilles des partitions qu'il faut assembler en une seule image
+      # pour la découper ensuite
+      # NON : on procède par page et on assemble tout à la fin
+      Ajax << {error: "On ne peut pas encore fournir de dossier."}
 
-    score_ini_name = "score_ini#{File.extname(score_ini_path)}"
+    else
 
-    # On enregistre ce path dans les données du score
-    Score.set(score_ini_path: score_ini_path)
-    
-    dst = File.join(SCORE_FOLDER,score_ini_name)
-    FileUtils.copy(score_ini_path, dst)
-    Ajax << {
-      message: "Le chemin d'accès à la partition a été enregistré.",
-      score_ini_name: score_ini_name
-    }
+      score_ini_name = "score_ini#{File.extname(score_ini_path)}"
+
+      # On enregistre ce path dans les données du score
+      Score.set(score_ini_path: score_ini_path)
+
+      dst = File.join(SCORE_FOLDER,score_ini_name)
+      FileUtils.copy(score_ini_path, dst)
+      Ajax << {
+        message: "Le chemin d'accès à la partition a été enregistré.",
+        score_ini_name: score_ini_name
+      }
+    end
   else
     Ajax << {error: "La partition '#{score_ini_path}' est introuvable…"}
   end
