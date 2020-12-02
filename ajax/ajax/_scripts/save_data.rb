f9b9dada158c64b4983f75e02d1100ebe051d364
Philippe Perret <philippe.perret@yahoo.fr>
2020-12-02 10:38:12 +0100

    Surveillance de l'extension de la partition originale

--- ajax/ajax/_scripts/save_data.rb
+++ ajax/ajax/_scripts/save_data.rb
@@ -8,6 +8,10 @@ begin
   data = Ajax.param(:data).to_sym
   if CURRENT_ANALYSE == nil || CURRENT_ANALYSE.empty?
     Ajax << { error: "Aucune analyse n'est définie. Consultez l'aide pour savoir comment la définir."}
+  elsif data[:score_ini_path].nil?
+    Ajax << {error: "Il faut définir le chemin d'accès à la partition originale."}
+  elsif not ['.jpg','.png','.jpeg','.tiff'].include?(File.extname(data[:score_ini_path]))
+    Ajax << {error: "La partition originale doit absolument être un fichier image. Or son extension est '#{File.extname(data[:score_ini_path])}'…"}
   else
     score = Score.new(CURRENT_ANALYSE)
     if not(score.exists?)
