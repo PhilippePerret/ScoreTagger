618c60a5e89e0ef5a9eb58dd3a9d7f60ecad0de0
Philippe Perret <philippe.perret@yahoo.fr>
2020-12-02 12:35:11 +0100

    Ajout des scripts ajax oubliés

---
+++ ajax/ajax/_scripts/crop_score/crop.rb
@@ -0,0 +1,33 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+=begin
+  Ce module permet de produire les systèmes séparés de la partition
+=end
+
+# CROP_LINES_DATA = [[418, 209],[627, 179],[806, 203],[1009, 198],[1207, 204],[1411, 239]]
+
+# Ligne pour extraire le système de la partition originale
+CODE_CROP_IMAGE = "/usr/local/bin/convert #{SCORE.relative_page_path(PAGE)} -crop 0x%{height}+0+%{top} ./factory/system-#{PAGE}-%{iimg}.jpg"
+
+def proceed_crop_score_ini
+  FileUtils.mkdir_p(FACTORY_FOLDER)
+
+  # Produire le code qui va découper
+  code_decoupe = []
+  CROP_LINES_DATA.each_with_index do |dline, idx|
+    dline = dline.merge(iimg: idx.to_s.rjust(3,'0')).to_sym
+    log("dline: #{dline.inspect}")
+    code_decoupe << CODE_CROP_IMAGE % dline
+  end
+
+  code_decoupe = <<-BASH
+cd "#{SCORE.folder}"
+#{code_decoupe.join("\n")}
+
+BASH
+  log("Code de découpe:\n#{code_decoupe}")
+  # res = `#{code_decoupe}\n`
+  res = system("#{code_decoupe}\n")
+
+  return res
+end #/ proceed_crop_score_ini
